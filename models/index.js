fa798165be6a6b7dfe0403c7d08a7d5eb34c7f7f
Matt Hooper <mhooper72@gmail.com>
2017-12-04 17:10:13 +0000

    mLab connection

--- models/index.js
+++ models/index.js
@@ -3,9 +3,13 @@ var mongoose = require('mongoose');
 // debug true - see info in console
 mongoose.set('debug', true);
 // connect to mongodb
-mongoose.connect('mongodb://localhost/todo-api');
+
+mongoose.connect('mongodb://hoop71:asdfasdf1@ds129776.mlab.com:29776/node-todo-mh');
+// mongoose.connect('mongodb://localhost/todo-api');
+// mongodb://hoop71:asdfasdf1@ds129776.mlab.com:29776/node-todo-mh
 
 // allows to use Promise syntax
 mongoose.Promise = Promise; 
 
 module.exports.Todo = require("./todo");
+
